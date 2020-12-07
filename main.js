324181f070360ce690a8f1585797e32ca1893831
Ryan <ryan.eliopoulos@gmail.com>
2020-12-06 21:09:52 -0800

    -Old Django PoC
    Removed the directory containing the first django PoC developed.
    Replaced with the contents of the mysite directory.

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
+/*
+
+  Register event listener so the user can "submit" moves, but client-side validation on whether it's
+  actually our turn or not for the sake of simplicity.
+
+ */
 
 
 const fetch = require('node-fetch')
+
+MOVE_API_ENDPOINT   = 'http://138.68.18.203:8002/polls/api_makemove'
+STATECHECK_ENDPOINT = 'http://138.68.18.203:8002/polls/api_checkstate'
+
 
 class View {
 
@@ -16,40 +26,49 @@ class View {
       this.our_turn = true;
     }
 
-    // Call the fetch function here to get things going?
+    // Kick things off waiting for the opponent to make a move
     if (!this.our_turn) {
-      console.log('It isn\'t our turn');
       this.poll_gamestate();
     }
   }
 
- async poll_gamestate() {
+
+  async poll_gamestate() {
 
     function process_server_success(data, view) {
       // This should be receiving json formatted data subsequent to a successful
       // response from the game state endpoint
+
+      // Checking if a move has been made
       view.current_player = data['current_player']
       if (view.current_player === view.username) {
         view.our_turn = true;
       }
+
       // Calculate timedelta from last_timestamp to calculate and
       // set the time_remaining in our turn.
       // delta = datetime.now() - data['last_timestamp']
       // this.time_remaining = delta
-      view.pending_promise = false;
       console.log('This is our data:', data);
 
+      // Update the html elementById S1 through S9 with the values held in data.
+      view.pending_promise = false;
       // Now set up the next api call if we are still waiting on the opponent
       if (!view.our_turn) {
-        console.log('it\'s still our turn..setting up to query the api in another 5 seconds');
+        console.log('it\'s still not our turn..setting up to query the api in another 5 seconds');
         setTimeout(view.poll_gamestate(), 5000);
       }
+
    }
    function process_server_failure(error, view) {
       // Ending up here when the fetch call
       // results in an error.
+
       view.pending_promise = false;
       console.log('Encountered the following error:', error)
+
+     // At this point I could just continue looping to a request against the server
+     // but I want a mechanism that prevents an infinite loop of failed promises.
    }
 
     if (!this.pending_promise) {
@@ -61,10 +80,36 @@ class View {
     }
  }
 
+ async submit_move(data) {
+    console.log('hello!')
+  }
+
 }
 
 
 var ourview = new View('zeratul', 'X', 'kerrigan', 30);
 
 console.log('here');
+
+
+
+
+
+/*
+
+  useful code for testing gameplay state changes through the console
+  before implementing the UI further.
+
+ */
+
+// const stdin = process.openStdin()
+//
+// process.stdout.write('Enter name: ')
+//
+// stdin.addListener('data', text => {
+//   const name = text.toString().trim()
+//   console.log('Your name is: ' + name)
+//
+// })
+//
 
